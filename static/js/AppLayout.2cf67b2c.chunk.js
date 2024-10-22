fe2e3daa6d4be99f28871d711c1d776b58899280
aakashnegi97 <61917637+aakashnegi97@users.noreply.github.com>
2024-10-22 15:40:37 +0530

    Updates

--- static/js/AppLayout.2cf67b2c.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkfanizm=self.webpackChunkfanizm||[]).push([[670],{819:(s,e,a)=>{a.r(e),a.d(e,{default:()=>f});a(43);var n=a(330),c=a(579);const f=()=>(0,c.jsx)(c.Fragment,{children:(0,c.jsx)(n.sv,{})})}}]);
-//# sourceMappingURL=AppLayout.2cf67b2c.chunk.js.map
